b44d1f9b902e4422fdf0934b0974f78a992674bd
Iacopo Vagliano <iacopo.vagliano@polito.it>
2015-05-21 17:33:39 +0200

    completed class inventory and added test packages

--- inventory/src/it/polito/inventory/Inventory.java
+++ inventory/src/it/polito/inventory/Inventory.java
@@ -7,32 +7,86 @@ public class Inventory{
 	private Map<String, Item> items = new HashMap<String, Item>();
 	
 	// adds new descriptor 
-	void addItem(Item i) throws ItemAlreadyExists{
+	void addItem(Item i) throws ItemAlreadyExists {
+		
+		if (items.putIfAbsent(i.getItemCode(), i) == null) {
+			throw new ItemAlreadyExists();
+		}
 		
 	}
 	
 	// returns item with given code
     Item searchItem (String itemCode) throws ItemNotExists {
-		return null;
+		
+    	Item i = items.get(itemCode);
+		
+    	if (i == null) {
+			throw new ItemNotExists();
+		}
+    	
+    	return i;
     	
     }
     
     // returns availability of item
     int availabilityItem (String itemCode) throws ItemNotExists {
-		return 0;
+    	
+    	Item i = items.get(itemCode);
+    	
+		if (i == null) {
+			throw new ItemNotExists();
+		}
+		
+		return i.getAvailability();
     	
     }
     
     // subtracts 1 to availability
     void subtractItem (String itemCode) throws ItemNotExists, ItemNotAvailable {
+
+    	Item i = items.get(itemCode);
     	
+		if (i == null) {
+			throw new ItemNotExists();
+		}
+		
+		if (i.getAvailability() <= 0) {
+			throw new ItemNotAvailable();
+		}
+		
+		i.setAvailability(i.getAvailability() - 1);
+		
     }
                                                            
-    void addQtyToItem(String itemCode, int qty_to_add) {
+    void addQtyToItem(String itemCode, int qty_to_add) throws ItemNotExists {
+
+    	Item i = items.get(itemCode);
     	
+		if (i == null) {
+			throw new ItemNotExists();
+		}
+		
+		i.setAvailability(i.getAvailability() + qty_to_add);
     }
     
-    void  subtractQtyToItem(String itemCode, int qty_to_add) {
+    void  subtractQtyToItem(String itemCode, int qty_to_add) 
+    		throws ItemNotAvailable, ItemNotExists {
+
+    	Item i = items.get(itemCode);
     	
+		if (i == null) {
+			throw new ItemNotExists();
+		}
+		
+		if (i.getAvailability() <= 0) {
+			throw new ItemNotAvailable();
+		}
+		
+		//not check if availability goes under 0!
+		i.setAvailability(i.getAvailability() - qty_to_add);
     }
+
+	public Map<String, Item> getItems() {
+		return items;
+	}
 }
